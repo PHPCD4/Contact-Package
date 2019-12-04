34ceec57362c083ecb13df682351bd1f0433a101
Golam Sorwar <akibmd719@gmail.com>
2019-12-04 22:40:25 +0600

    remove validation

--- src/Http/Controllers/ContactController.php
+++ src/Http/Controllers/ContactController.php
@@ -16,18 +16,6 @@ class ContactController extends Controller
 
     public function send(Request $request)
     {
-        $validator = \Validator::make($request->all(), [
-            'name'=>'required',
-            'email'=>'required|email',
-            'message'=>'required'
-        ]);
-
-        if ($validator->fails()) {
-            return redirect('contact')
-                        ->withErrors($validator)
-                        ->withInput();
-        }
-
     	\Mail::to(config('contact.send_email_to'))->send(new ContactMailable($request->name,$request->message));
     	Contact::create($request->all());
 
